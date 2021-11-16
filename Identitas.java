4408581b07a55b65066d649cff20e91baa8600ff
banisaputra <55937017+Banisaputra@users.noreply.github.com>
2021-11-16 19:44:25 +0700

    Add files via upload

---
+++ Identitas.java
@@ -0,0 +1,7 @@
+public class Identitas {
+	public static void main (String [] args){
+		System.out.println ("Shafa Bani Saputra");
+		System.out.println ("L200190151");
+		System.out.println ("Saya akan Lulus kuliah daring ini");
+	}
+}
