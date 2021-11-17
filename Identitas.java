de8e7183ea611d5cee34ab9fd617b011d9931e43
banisaputra <55937017+Banisaputra@users.noreply.github.com>
2021-11-17 19:52:40 +0700

    Update Identitas.java

--- Identitas.java
+++ Identitas.java
@@ -1,7 +1,7 @@
 public class Identitas {
 	public static void main (String [] args){
-		System.out.println ("Shafa Bani Saputra");
-		System.out.println ("L200190151");
-		System.out.println ("Saya akan Lulus kuliah daring ini");
+		System.out.println ("Bani Saputra");
+		System.out.println ("200190151");
+		System.out.println ("Saya akan Lulus");
 	}
 }
