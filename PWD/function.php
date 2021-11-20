d2a3a9486bda000c375d29e7513ee4b4abccba54
banisaputra <55937017+Banisaputra@users.noreply.github.com>
2021-11-20 08:16:00 +0700

    PWD
    Pertemuan5

--- PWD/function.php
+++ PWD/function.php
@@ -1,47 +1,14 @@
 <?php
-// doctype bns author
-$conn = mysqli_connect('localhost','root','','latihanuts');
+$conn = mysqli_connect('localhost','root','','universitas');
 
-function tambah($data){
+function query($query) {
 	global $conn;
-	// ambil data dari tiap element dalam form
-	$noktp = htmlspecialchars($data['noktp']);
-    $nama = htmlspecialchars($data['nama']);
-    $alamat = htmlspecialchars($data['alamat']);
-    $gender = htmlspecialchars($data['gender']);
-    $tmpLahir = htmlspecialchars($data['tmpLahir']);
-    $tglLahir = htmlspecialchars($data['tglLahir']);
-    $status = htmlspecialchars($data['status']);
-    $pulau = htmlspecialchars($data['pulau']);
-
-    $query = "INSERT INTO penduduk VALUES ('$noktp','$nama','$alamat','$gender','$tmpLahir','$tglLahir','$status','$pulau')";
-	mysqli_query($conn, $query);
+	$result = mysqli_query($conn, $query);
+	$rows = [];
+	while ($row = mysqli_fetch_assoc($result)){
+		$rows[] = $row;
+	}
+	return $rows;
 }
-
-function hapus($noktp){
-	global $conn;
-	mysqli_query($conn, "DELETE FROM penduduk WHERE no_ktp ='$noktp'");
-	return mysqli_affected_rows($conn);
-}
-
-function ubah($data) {
-	global $conn;
-	// ambil data dari tiap element dalam form
-	$noktp = htmlspecialchars($data['noktp']);
-    $nama = htmlspecialchars($data['nama']);
-    $alamat = htmlspecialchars($data['alamat']);
-    $gender = htmlspecialchars($data['gender']);
-    $tmpLahir = htmlspecialchars($data['tmpLahir']);
-    $tglLahir = htmlspecialchars($data['tglLahir']);
-    $status = htmlspecialchars($data['status']);
-    $pulau = htmlspecialchars($data['pulau']);
-
-    $query = "UPDATE penduduk SET no_ktp='$noktp', nama='$nama',alamat='$alamat',jenis_kelamin='$gender',tempat_lahir='$tmpLahir',
-    tanggal_lahir='$tglLahir',status_wn='$status',pulau='$pulau' WHERE no_ktp='$noktp'";
-	mysqli_query($conn, $query);
-
-	return mysqli_affected_rows($conn);
-}
-
 
 ?>
--- PWD/index.php
+++ PWD/index.php
@@ -1,69 +1,42 @@
-<?php 
-require 'functions.php';
+<?php
+require "function.php";
 
 $mahasiswa = query ("SELECT * FROM mahasiswa");
 
 ?>
 
-
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Halaman Admin</title>
-	<style>
-		.loader {
-			width: 80px;
-			position: absolute;
-			top: 130px;
-			left: 350px;
-			z-index: -1;
-			display: none;
-		}
-		@media print {
-			.logout, .tambah, .form-cari, .aksi{
-				display: none;
-			}
-		}
-	</style>
+	<title>Data Mahasiswa</title>
 </head>
 <body>
-
-<h1>Daftar Mahasiswa</h1>
-<a href="tambah.php" class="tambah">Tambah Data Mahasiswa</a>
-<br><br>
-<div id='container'>
-<br>
-<table border="1" cellpadding="10" cellspacing="0">
-	<tr>
-		<th>No.</th>
-		<th class="aksi">Aksi</th>
-		<th>Gambar</th>
-		<th>NIM</th>
-		<th>Nama</th>
-		<th>Email</th>
-		<th>Jurusan</th>
-	</tr>
-	<?php $no = 1; ?>
-	<?php foreach($mahasiswa as $row) : ?>
+	<h1>Daftar Mahasiswa</h1>
+	<table border='1' cellpadding='20' cellspacing='0'>
+		<tr>
+			<th>No.</th>
+			<th>Aksi</th>
+			<th>NIM</th>
+			<th>Nama</th>
+			<th>Email</th>
+			<th>Jurusan</th>
+			<th>Gambar</th>
+		</tr>
+		<?php $i = 1; ?>
+		<?php foreach ($mahasiswa as $row) : ?>
+		<tr>
+			<td><?= $i; ?></td>
+			<td><a href = "edit.php">Edit</a> |
+				<a href = "delete.php">Delete</a></td>
+			<td><?= $row ["nim"]; ?></td>
+			<td><?= $row ["nama"]; ?></td>
+			<td><?= $row ["email"]; ?></td>
+			<td><?= $row ["jurusan"]; ?></td>
+			<td><img src = "images/<?= $row ['gambar']; ?>" width = "50" height ="50"></td>
+		</tr>
+		<?php $i++; ?>
+		<?php endforeach; ?>
 		
-	<tr>
-		<td><?= $no; ?></td>
-		<td class="aksi">
-			<a href="ubah.php?id=<?= $row["id_mhs"]; ?>">ubah</a> | <a href="hapus.php?id=<?= $row["id_mhs"]; ?>" onclick="return confirm('apakah anda yakin ingin menghapus?');">hapus</a>
-		</td>
-		<td><img src="img/<?= $row["gambar"]; ?>" width="50" height="50"></td>
-		<td><?= $row["nim"]; ?></td>
-		<td><?= $row["nama"]; ?></td>
-		<td><?= $row["email"]; ?></td>
-		<td><?= $row["jurusan"]; ?></td>
-	</tr>
-	<?php $no++; ?>
-	<?php endforeach; ?>
-
-</table>
-</div>
-
-<script src="js/jquery-3.5.1.min.js"></script>
-<script src="js/script.js"></script>
 </body>
 </html>
+
