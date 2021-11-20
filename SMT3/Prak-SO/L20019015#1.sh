982fbf65e03a46fb01be2863f8852a1afe6ddea7
banisaputra <55937017+Banisaputra@users.noreply.github.com>
2021-11-20 09:54:20 +0700

    UAS
    Ujian Akhir Semester 3 Pemprograman Sistem Operasi

---
+++ SMT3/Prak-SO/L20019015#1.sh
@@ -0,0 +1,13 @@
+WAKTU="Tanggal dan Jam saat ini : \c"
+JMLUSER="Jumlah user : \c"
+AKU="Status Personal : \c"
+echo  "$WAKTU"
+date
+echo  "$JMLUSER"
+who|wc -l
+echo  "$AKU"
+whoami
+
+echo "Hello World!"
+echo "I'm using Linux"
+exit 0
---
+++ SMT3/Prak-SO/L200190151.sh
@@ -0,0 +1,18 @@
+NameCom="Computer Name : \c"
+InfOS="OS saat ini : \c"
+LastUsr="User Login terakhir : \c"
+UsrName="User Name : \c"
+ID="ID : \c"
+
+echo "Assalamualaikum"
+echo "$NameCom"
+hostname
+echo "$InfOS"
+uname
+echo "$LastUsr"
+who
+echo "$UsrName"
+whoami
+echo "$ID"
+id
+exit 0
